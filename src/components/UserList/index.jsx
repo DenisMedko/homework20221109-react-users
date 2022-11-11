bba1522bc3f4b3afab9277d83f0f69c478d9813e
DenisMedko <medko.denis76@gmail.com>
2022-11-11 11:33:23 +0200

    users data transferred from component to file

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import User from '../User';
+import {users} from '../../data/users.js';
 
 class UserList extends Component {
     state = {
-        users : [
-            {id: 1, firstName : 'Denys', lastName : 'Medko', likes : 99, myLike : false},
-            {id: 2, firstName : 'Oksana', lastName : 'Zhukova', likes : 199, myLike : false},
-            {id: 3, firstName : 'Nika', lastName : 'Medko', likes : 299, myLike : false},
-            {id: 4, firstName : 'Max', lastName : 'Kornev', likes : 0, myLike : false},
-            {id: 5, firstName : 'Ramzes', lastName : 'The Second', likes : 599, myLike : false},
-        ], 
+        users : users, 
     };
     likeHandle = (id) => {
         const newUsers = this.state.users.map(user => {
---
+++ src/data/users.js
@@ -0,0 +1,9 @@
+const usersArray = [
+    {id: 1, firstName : 'Denys', lastName : 'Medko', likes : 99, myLike : false},
+    {id: 2, firstName : 'Oksana', lastName : 'Zhukova', likes : 199, myLike : false},
+    {id: 3, firstName : 'Nika', lastName : 'Medko', likes : 299, myLike : false},
+    {id: 4, firstName : 'Max', lastName : 'Kornev', likes : 0, myLike : false},
+    {id: 5, firstName : 'Ramzes', lastName : 'The Second', likes : 599, myLike : false},
+]
+
+export const users = usersArray;
